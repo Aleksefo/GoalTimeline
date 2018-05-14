2dfd0d46d4991e09e9eb83afc39b3f937405c03c
Alex Fomushkin <alex.fomushkin@honkio.com>
2018-05-14 12:50:56 +0300

    Add localization and styles

---
+++ src/_values/strings.js
@@ -0,0 +1,6 @@
+import LocalizedStrings from 'react-native-localization'
+
+//todo sort lines by class
+const strings = new LocalizedStrings({
+	en: {}
+})
---
+++ src/_values/styles.js
@@ -0,0 +1,7 @@
+export const colors = {
+	primary: '#7f0080',
+	accent: '#fcd036',
+	inactive: '#cacaca',
+	danger: '#D2473C',
+	// iconSize: 24
+}
